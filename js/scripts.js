9376944d1e280b090fdc0820bfca0d2a89589817
emmanuel <eoropezasp@gmail.com>
2023-08-21 12:29:33 -0300

    inicio de proyecto + mune responsive

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+const toggleMenuElement = document.getElementById('toggle-menu');
+const mainMenuElement = document.getElementById('main-menu');
+const mainNavElement = document.getElementById('main-nav');
+// const mainsectionElement = document.getElementById('caminantes');
+
+
+toggleMenuElement.addEventListener('click', () => {
+    mainNavElement.classList.toggle('main-nav--show');
+    mainMenuElement.classList.toggle('main-menu--show');
+    
+});
+
+
+
+
+// var coll = document.getElementsByClassName("toggle-menu");
+// var i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
