1069b6564b874b1d5f71beace2e4dab543b8c6ee
emmanuel <eoropezasp@gmail.com>
2023-08-23 09:40:56 -0300

    nabvar responsibe terminado

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,15 @@
-
-
-
 var coll = document.getElementsByClassName("toggle-menu");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+  coll[i].addEventListener("click", function () {
+    // this.classList.toggle("active"); asigna esta clase al elemento capturado en este caso por "coll"
     var content = this.nextElementSibling;
-    if (content.style.maxHeight){
+    if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-    } 
+    }
   });
 }
 
