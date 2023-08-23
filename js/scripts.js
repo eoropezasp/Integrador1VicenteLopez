cca8140b01249da4497dd39feb76789f2b7229e2
emmanuel <eoropezasp@gmail.com>
2023-08-23 09:04:58 -0300

    2da correccion

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,18 @@
-// const toggleMenuElement = document.getElementById('toggle-menu');
-// const mainMenuElement = document.getElementById('main-menu');
-// const mainNavElement = document.getElementById('main-nav');
-// // const mainsectionElement = document.getElementById('caminantes');
-
-
-// toggleMenuElement.addEventListener('click', () => {
-//     mainNavElement.classList.toggle('main-nav--show');
-//     mainMenuElement.classList.toggle('main-menu--show');
-    
-// });
 
 
 
+var coll = document.getElementsByClassName("toggle-menu");
+var i;
 
-// var coll = document.getElementsByClassName("toggle-menu");
-// var i;
-
-// for (i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var content = this.nextElementSibling;
-//     if (content.style.display === "block") {
-//       content.style.display = "none";
-//     } else {
-//       content.style.display = "block";
-//     }
-//   });
-// }
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
 
