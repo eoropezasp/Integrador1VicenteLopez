727aae57b56cf3efff66226d44d78874ea271f86
emmanuel <eoropezasp@gmail.com>
2023-08-26 14:28:26 -0300

    nav dinamico

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,49 @@
+
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+
+header.innerHTML = `
+<div>
+
+<div class="encabezado"></div>
+
+<div class="logo">
+    <img src="recursos/logo.png" alt="">
+</div>
+
+<div class="toggle-menu" id="toggle-menu">
+    <i class="fa-solid fa-bars fa-2xl" style="color: #ffffff;"></i>
+</div>
+
+<nav class="main-nav" id="main-nav">
+    <ul class="main-menu" id="main-menu">
+        <li class="main-menu__item">
+            <a href="#" class="main-menu__link">QUIENES SOMOS</a>
+        </li>
+        <li class="main-menu__item">
+            <a href="#" class="main-menu__link">PROYECTO</a>
+        </li>
+        <li class="main-menu__item">
+            <a href="#" class="main-menu__link">ACTIVIDADES</a>
+        </li>
+        <li class="main-menu__item">
+            <a href="#" class="main-menu__link">COLABORAR</a>
+        </li>
+        <li class="main-menu__item">
+            <a href="#" class="main-menu__link">INSCRIPCIONES</a>
+        </li>
+        <li class="main-menu__item">
+            <a href="#" class="main-menu__link"><i class="fa-brands fa-instagram fa-2xl"
+                    style="color: #ffffff;"></i></a>
+        </li>
+
+    </ul>
+</nav>
+
+</div>
+
+`;
+
 var coll = document.getElementsByClassName("toggle-menu");
 var i;
 
@@ -12,4 +58,3 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
-
