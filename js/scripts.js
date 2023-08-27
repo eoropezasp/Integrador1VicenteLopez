ac8238db800ce27dfc8134323caaab86133f3bf5
emmanuel <eoropezasp@gmail.com>
2023-08-26 22:51:50 -0300

    vista home base terminada

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ header.innerHTML = `
 <div class="encabezado"></div>
 
 <div class="logo">
-    <img src="recursos/logo.png" alt="">
+    <img src="./recursos/logo.png" alt="">
 </div>
 
 <div class="toggle-menu" id="toggle-menu">
@@ -43,6 +43,61 @@ header.innerHTML = `
 </div>
 
 `;
+
+
+footer.innerHTML =`
+
+<section class="container-footer">
+<div class="row">
+    <div class="contacto-datos col-md-6 mx-auto">
+    <div class="datos-titulo">
+        <h2>Escribinos</h2>
+        </div>
+        <div class="datos">
+            <p>Malvinas argentinas 1685</p>
+            <p>Gonzales Catan</p>
+            <p>Provincia de buenos aires</p>
+            <p>Tel. (54)5247 4525</p>
+            <p>info@blabla.com</p>
+            <div class="slogan">
+                <img src="./recursos/logoGC_slogan.webp" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="contacto-form px-5 col-md-6 mx-auto">
+        <form class="formulario">
+            <div class="form-group">
+                <label for="nombre">Nombre</label>
+                <input type="text" class="form-control" id="nombre" placeholder="Ej: Eduardo">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email</label>
+                <input type="email" class="form-control" id="exampleInputEmail1"
+                    aria-describedby="emailHelp" placeholder="Enter email">
+               
+            </div>
+            <div class="form-group">
+                <label for="mensaje">Mensaje</label>
+                <textarea class="form-control" id="mensaje" rows="3"></textarea>
+                <small class="form-text text-muted">We'll never share your email with anyone
+                    else.</small>
+            </div>
+
+
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+
+
+    </div>
+</div>
+
+
+
+</section>
+
+`;
+
+
 
 var coll = document.getElementsByClassName("toggle-menu");
 var i;
